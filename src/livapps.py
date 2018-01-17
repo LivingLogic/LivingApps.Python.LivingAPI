b5adfce874b646bbcad876b328f1bca7f843257d
Walter Doerwald <walter@livinglogic.de>
2018-01-17 15:17:30 +0100

    Simplify code.

--- src/livapps.py
+++ src/livapps.py
@@ -805,7 +805,7 @@ class Record(Base):
 	@property
 	def fields(self):
 		if self._fields is None:
-			self._fields = attrdict((control.identifier, Field(control, self, value)) for (control, value) in zip(self.app.controls.values(), self.values.values()))
+			self._fields = attrdict((identifier, Field(self.app.controls[identifier], self, value)) for identifier in self.app.controls)
 		return self._fields
 
 	def update(self, **kwargs):
