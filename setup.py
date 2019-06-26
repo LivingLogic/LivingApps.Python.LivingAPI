85e3f10d8019df30b1400eb9a2df152345ed6018
Walter Doerwald <walter@livinglogic.de>
2019-06-26 16:31:14 +0200

    Fix the code that creates the description.
    Read the correct news file (CHANGELOG.rst) and fix an f-string.

--- setup.py
+++ setup.py
@@ -30,14 +30,14 @@ LivingApps
 """
 
 try:
-	news = list(open("docs/NEWS.rst", "r", encoding="utf-8"))
+	news = list(open("CHANGELOG.rst", "r", encoding="utf-8"))
 except IOError:
 	description = DESCRIPTION.strip()
 else:
 	# Extract the first section (which are the changes for the current version)
 	underlines = [i for (i, line) in enumerate(news) if line.startswith("---")]
 	news = news[underlines[0]-1:underlines[1]-1]
-	description = "{DESCRIPTION.strip()}\n\n\n{}".format("".join(news))
+	description = f"{DESCRIPTION.strip()}\n\n\n{{}}".format("".join(news))
 
 # Get rid of text roles PyPI doesn't know about
 description = re.subn(":[a-z]+:`~?([-a-zA-Z0-9_./]+)`", "``\\1``", description)[0]
