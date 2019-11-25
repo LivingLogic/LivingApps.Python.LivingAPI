8e4bc141486d8a1227af3fc039afd91851bf8ab3
Walter Doerwald <walter@livinglogic.de>
2019-11-25 09:52:07 +0100

    More tests.

---
+++ test/test_vsql_binop_shiftleft.py
@@ -0,0 +1,32 @@
+"""
+Tests for the vSQL left shift operator ``<<``.
+
+The test are done via the Python DB interface.
+
+To run the tests, :mod:`pytest` is required.
+"""
+
+from conftest import *
+
+
+###
+### Tests
+###
+
+def test_bool_bool1(config_persons):
+	check_vsql(config_persons, "app.p_int_none.value << False is None")
+
+def test_bool_bool2(config_persons):
+	check_vsql(config_persons, "app.p_bool_false.value << True == 0")
+
+def test_bool_bool3(config_persons):
+	check_vsql(config_persons, "app.p_bool_true.value << True == 2")
+
+def test_bool_int(config_persons):
+	check_vsql(config_persons, "app.p_bool_true.value << 1 == 2")
+
+def test_int_bool(config_persons):
+	check_vsql(config_persons, "2 << app.p_bool_true.value == 4")
+
+def test_int_int(config_persons):
+	check_vsql(config_persons, "-app.p_int_value.value << 2 == -7108")
