50b5efdc375df4629148b69e82728e9e9472e5bd
Walter Doerwald <walter@livinglogic.de>
2025-01-13 17:24:37 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ description = description.expandtabs(2)
 
 args = dict(
 	name="ll-la",
-	version="0.39.1",
+	version="0.40.0",
 	description="Python API for LivingApps",
 	long_description=description,
 	author="Walter Doerwald",
