30d0d2d2f9dd91f1e598731b0b0711e825cb1192
Walter Doerwald <walter@livinglogic.de>
2025-12-15 19:59:23 +0100

    Update version number of the LivingAPI to 137.

--- src/ll/la/__init__.py
+++ src/ll/la/__init__.py
@@ -2193,7 +2193,7 @@ class Globals(CustomAttributes):
 	})
 	ul4_type = ul4c.Type("la", "Globals", "Global information")
 
-	supported_version = "136"
+	supported_version = "137"
 
 	class Mode(misc.Enum):
 		"""
